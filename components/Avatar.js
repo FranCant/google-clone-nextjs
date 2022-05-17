98052af7277b75253082f2f9db5f7a9742df0c13
Fran <cantorifrancisco@gmail.com>
2022-05-17 12:54:06 +0200

    avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 function Avatar({ url, className }) {
   return (
-    <div className={`flex items-center rounded-full cursor-pointer bg-indigo-400 ${className}`}>
+    <div className={`overflow-hidden flex items-center rounded-full cursor-pointer transition duration-150 hover:scale-110 ${className}`}>
       <Image
-        className={`rounded-full cursor-pointer transition duration-150 hover:scale-110`}
         height={35}
         width={35}
         src={url}
