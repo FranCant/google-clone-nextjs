dd449575abbe2f5e535d46f0eaf914050012f8b7
Fran <cantorifrancisco@gmail.com>
2022-05-19 07:26:18 +0200

    Bug Fix

--- pages/index.js
+++ pages/index.js
@@ -52,8 +52,8 @@ export default function Home() {
       <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
         <Image
           src="https://www.google.es/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-          height={100}
-          width={300}
+          height={80}
+          width={240}
           alt="profile"
         />
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
